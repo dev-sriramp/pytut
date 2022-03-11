ddff3a0c37d9b4f771edc15380ec8db81f0df3bc
SRIRAM <uniqueredhat@gmail.com>
2022-03-11 20:42:59 +0530

    Update STatusBar.js

--- src/screens/STatusBar.js
+++ src/screens/STatusBar.js
@@ -1,1 +1,12 @@
 import React from 'react';
+const STatusBar = () =>{
+return(
+ <StatusBar
+                    barStyle='dark-content'
+                    hidden={false}
+                    backgroundColor={APP_WHITE}
+                    translucent={false}
+                    networkActivityIndicatorVisible={true}
+                />
+)
+}
